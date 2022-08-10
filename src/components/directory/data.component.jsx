13a6230fb51893637e7940ea41540e00f6cc004d
Jiani Liu <liujianiljn@gmail.com>
2022-08-10 11:48:10 -0700

    Finished the homepage layout. Update the componenets.

---
+++ src/components/directory/data.component.jsx
@@ -0,0 +1,28 @@
+const Data = [
+    {
+      "id": 1,
+      "title": "Skincare",
+      "imageUrl": "https://images.unsplash.com/photo-1590393802710-dbf451560939?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60"
+    },
+    {
+      "id": 2,
+      "title": "Face Makeup",
+      "imageUrl": "https://images.unsplash.com/photo-1557205465-f3762edea6d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm91bmRhdGlvbiUyMG1ha2V1cHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
+    },
+    {
+      "id": 3,
+      "title": "Eye Makeup",
+      "imageUrl": "https://images.unsplash.com/photo-1625093525885-282384697917?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZXllc2hhZG93JTIwcGFsZXR0ZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
+    },
+    {
+      "id": 4,
+      "title": "Lip Makeup",
+      "imageUrl": "https://images.unsplash.com/photo-1631214524049-0ebbbe6d81aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGZvdW5kYXRpb24lMjBtYWtldXB8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
+    },
+    {
+      "id": 5,
+      "title": "Makeup Tool",
+      "imageUrl": "https://images.unsplash.com/photo-1592136957897-b2b6ca21e10d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80"
+    }
+  ];
+  export default Data;
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,13 @@
+import './directory.styles.scss';
+import Data from './data.component';
+import CategoryItem from "../catergory-item/category-item.component";
+const Directory = () => {
+  return (
+    <div className="directory-container">
+      {Data.map((category) => (
+        <CategoryItem key={category.id} category={category}/>
+      ))}
+    </div>
+  );
+}
+export default Directory;
