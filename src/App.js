fc04a7951b1c6155bf5d784052980536cb0571fc
Jiani Liu <liujianiljn@gmail.com>
2022-08-14 22:16:37 -0700

    Add shop page and product card.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import {Routes,Route} from 'react-router-dom';
 import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.componenet';
 import Authentification from './routes/authentification/authentification.componenet';
-const Shop = () => {
-  return (
-    <h1>Shop Page</h1>
-  );
-}
-
+import Shop from './routes/shop/shop.componenet'
 const App = () => {
   return (
     <Routes>
