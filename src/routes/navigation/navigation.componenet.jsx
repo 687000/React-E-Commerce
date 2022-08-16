4edfad9f3771039d1ccbee3e5ea6f7560ada037f
Jiani Liu <liujianiljn@gmail.com>
2022-08-15 22:15:23 -0700

    Add cart icon and container

--- src/routes/navigation/navigation.componenet.jsx
+++ src/routes/navigation/navigation.componenet.jsx
@@ -4,6 +4,8 @@ import {Outlet,Link} from 'react-router-dom';
 import { ReactComponent as CrwLogo} from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.componenet';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.componenet';
 import './navigation.styles.scss'
 const Navigation=()=>{
   const {currentUser}=useContext(UserContext);
@@ -17,7 +19,9 @@ const Navigation=()=>{
                 <Link className='nav-link' to='/shop'>SHOP</Link>
                 {currentUser?(<span className='nav-link' onClick={signOutUser}>SIGN OUT</span>):
                 (<Link className='nav-link' to='/auth'>SIGN IN</Link>)}
+                <CartIcon/>
             </div>
+            <CartDropdown/>
         </div>
         <Outlet/>
       </Fragment>
