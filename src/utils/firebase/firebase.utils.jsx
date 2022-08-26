43a767ddea96b91293dc8bc512f7204c9992e0ab
Jiani Liu <liujianiljn@gmail.com>
2022-08-25 22:05:29 -0700

    Enable to store data to firebase and use collections from firebase

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -8,12 +8,17 @@ import {
     signInWithEmailAndPassword,
     signOut,
     onAuthStateChanged
+
 } from 'firebase/auth'
 import {
     getFirestore,
     doc,
     getDoc,
-    setDoc
+    setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -46,7 +51,27 @@ const firebaseConfig = {
   export const signInWithGooglePopup=()=>signInWithPopup(auth, googleProvider);
   export const signInWithGoogleRedirect=()=>signInWithRedirect(auth, googleProvider);
   export const db= getFirestore();
-
+  export const addCollectionAndDocuments=async(collectionKey,objectsToAdd,field)=>{
+    const collectionRef=collection(db,collectionKey);
+    const batch=writeBatch(db);
+    objectsToAdd.forEach((object)=>{
+        const docRef=doc(collectionRef,object[field].toLowerCase());
+        batch.set(docRef,object);
+    });
+    await batch.commit();
+    console.log("done");
+  }
+  export const getCategoriesAndDocuments=async()=>{
+    const collectionRef=collection(db,'categories');
+    const q=query(collectionRef);
+    const querySnaoshot=await getDocs(q);
+    const categoryMap=querySnaoshot.docs.reduce((acc,docSnapshot)=>{
+        const {title,items}=docSnapshot.data();
+        acc[title.toLowerCase()]=items;
+        return acc;
+    },{})
+    return categoryMap;
+  }
   export const createUserDocumentFromAuth=async(userAuth,additionalInformation={})=>{
     if(!userAuth) return;
     const userDocRef = doc(db,'users',userAuth.uid);
