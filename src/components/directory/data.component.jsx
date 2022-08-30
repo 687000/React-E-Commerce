6434e00a1c8281b6db9cc693fe59664ce557aaa8
Jiani Liu <liujianiljn@gmail.com>
2022-08-29 22:26:18 -0700

    Added redirects for netlify deploy

--- src/components/directory/data.component.jsx
+++ src/components/directory/data.component.jsx
@@ -1,28 +1,28 @@
 const Data = [
-    {
-      "id": 1,
-      "title": "Skincare",
-      "imageUrl": "https://images.unsplash.com/photo-1631730486572-226d1f595b68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80"
-    },
-    {
-      "id": 2,
-      "title": "Face Makeup",
-      "imageUrl": "https://images.unsplash.com/photo-1557205465-f3762edea6d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm91bmRhdGlvbiUyMG1ha2V1cHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
-    },
-    {
-      "id": 3,
-      "title": "Eye Makeup",
-      "imageUrl": "https://images.unsplash.com/photo-1625093525885-282384697917?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZXllc2hhZG93JTIwcGFsZXR0ZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
-    },
-    {
-      "id": 4,
-      "title": "Lip Makeup",
-      "imageUrl": "https://images.unsplash.com/photo-1631214524049-0ebbbe6d81aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGZvdW5kYXRpb24lMjBtYWtldXB8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
-    },
-    {
-      "id": 5,
-      "title": "Makeup Tool",
-      "imageUrl": "https://images.unsplash.com/photo-1592136957897-b2b6ca21e10d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80"
-    }
-  ];
-  export default Data;
+  {
+    "id": 1,
+    "title": "hats",
+    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+  },
+  {
+    "id": 2,
+    "title": "jackets",
+    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+  },
+  {
+    "id": 3,
+    "title": "sneakers",
+    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+  },
+  {
+    "id": 4,
+    "title": "womens",
+    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+  },
+  {
+    "id": 5,
+    "title": "mens",
+    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+  }
+]
+export default Data;
