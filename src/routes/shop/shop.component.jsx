3e4aba98130eb61ee5002353786c42fa4124dfbd
Jiani Liu <liujianiljn@gmail.com>
2022-10-03 18:01:20 -0700

    Applied thunk to category in store

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,20 +1,15 @@
 import {Routes,Route}from 'react-router-dom';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories} from '../../store/categories/category.action';
-const Shop=()=>{
-    const dispatch=useDispatch();
-    useEffect(()=>{
-        const getCategoriesMap=async()=>{
-            const categoriesArray=await getCategoriesAndDocuments('categories');
-            dispatch(setCategories(categoriesArray));
-        }
-        getCategoriesMap();
-    },[dispatch])
+import { fetchCategoriesStartAsync } from '../../store/categories/category.action';
+const Shop = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(fetchCategoriesStartAsync());
+    }, []);
+  
     return ( 
     <Routes>
         <Route index element={<CategoriesPreview />}/>
--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { compose,createStore,applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import { rootReducer } from './root-reducer';
 //middleWares, run before actions hit reducer.
 
@@ -10,16 +11,19 @@ const persistConfig ={
     storage,
     blacklist:['user'],
 }
-const persistedReducer=persistReducer(persistConfig,rootReducer);
-const middleWares=[process.env.NODE_ENV!=="production"&&logger].filter(Boolean);
-
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const middleWares=[process.env.NODE_ENV!=="production"&&logger,thunk].filter(Boolean);
 
 //const middleWares=[loggerMiddleware]
 //compose, pass multipla function left to right
-const composedEnhancer=(process.env.NODE_ENV!=="production"&&window&&window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-const composedEnhancers=composedEnhancer(applyMiddleware(...middleWares))
+const composeEnhancer=(process.env.NODE_ENV!=="production"&&window&&window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composedEnhancers=composeEnhancer(applyMiddleware(...middleWares));
 //logger-what state look like before dispatch, what dispatch does, and how dispatch impact state
-export const store=createStore(persistedReducer,undefined,composedEnhancers);
+export const store = createStore(
+    persistedReducer,
+    undefined,
+    composedEnhancers
+  );
 //rootR educer, addtional default states, and logger(throught composedEnhancers)
 
 export const persistor=persistStore(store);
